07719c8da5551e8956e49eda4dc51b81bd99d8ba
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2026-03-09 13:00:34 +0200

    feat: add workflow at attendance permission #30

--- discipline_hr/hooks.py
+++ discipline_hr/hooks.py
@@ -232,6 +232,8 @@ fixtures = [
         "dt": "Notification",
         "filters": [["name", "=", "Attendance Penalty Pending Review"]],
     },
+    {"dt": "Workflow State"},
+    {"dt": "Workflow", "filters": [["name", "in", ["Attendance Permission"]]]},
 ]
 
 export_python_type_annotations = True
